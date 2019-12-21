56a021a19c54d8e4bc33291278f6f3ccbacf08a7
Cass Pangell <projectxcappe@Casss-MacBook-Pro.local>
2019-12-21 14:55:15 -0700

    Added more decending intervals to json.

--- Intervals/ViewController.swift
+++ Intervals/ViewController.swift
@@ -117,45 +117,6 @@ class ViewController: UIViewController, SettingsDelegate {
         selectedMethodsLabel.text = methodsString
     }
     
-    //Gamify and Progress Bar
-    func updateProgress(status:Answer) {
-        
-        switch status {
-        case .Correct:
-                CURR_ANSWER += 0.1
-        case .Incorrect:
-            if CURR_ANSWER >= 0.3 {
-                CURR_ANSWER -= 0.3
-            } else {
-                CURR_ANSWER = 0
-            }
-        case .Setup:
-            CURR_ANSWER = 1.0
-            progressView.setProgress(CURR_ANSWER, animated: false)
-            CURR_ANSWER = 0.0
-
-        }
-        
-        self.progressView.setProgress(self.CURR_ANSWER, animated: true)
-        
-        //Check to see if Max points and reset score and add next interval
-        if CURR_ANSWER >= MAX_ANSWER {
-            correctLabel.text = "BOOM!"
-            CURR_ANSWER = 0.0
-            
-            let currentIntervals = IntervalsSelected.shared.allIntervals!
-            selectedIntervals.append(currentIntervals[selectedIntervals.count])
-            
-            settingsDelegate?.updateSettings(updatedIntervals: selectedIntervals, updatedMethods: selectedMethods)
-            updateSettings()
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                self.progressView.setProgress(self.CURR_ANSWER, animated: true)
-            }
-            
-            
-        }
-    }
 
     func playInterval() {
         
